0aff478ca9a90d99b586ca903ab78a9b02ae264e
nkn-khr <nkn.khr@gmail.com>
2018-05-21 08:47:21 +0900

    #1に対する対応をおこないました

--- assessment.js
+++ assessment.js
@@ -43,7 +43,7 @@
         '{userName}のいいところは声です。{userName}の特徴的な声は云々',
         '{userName}のいいところは耳です。{userName}の特徴的なみみは云々',
         '{userName}のいいところは鼻です。{userName}の特徴的なハナは云々',
-        '{userName}のいいところはやさしさです。'
+        '{userName}のいいところはやさしさです。desu'
     ];
     function assessment(userName) {
         /**
